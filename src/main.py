b5f1af0171889a6005a5e6742ee3595230ac9593
Kenneth Bruce <me@kenneth-bruce.com>
2025-05-21 12:40:27 -0400

    basic project setup, added TextNode class

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextType, TextNode
+def main():
+    n = TextNode("Test normal text", TextType.NORMAL)
+    print(n)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODETEXT = "codetext"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text: str, text_type: TextType, url: str|None=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if (self.text_type == other.text_type) and (self.text == other.text) and (self.url == other.url):
+            return True
+        return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
