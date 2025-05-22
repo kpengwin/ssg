3459704e1b34f1dad5abba419abcdc01b29bef86
Kenneth Bruce <me@kenneth-bruce.com>
2025-05-22 10:29:20 -0400

    Simplified

--- src/textnode.py
+++ src/textnode.py
@@ -15,9 +15,7 @@ class TextNode():
         self.url = url
 
     def __eq__(self, other):
-        if (self.text_type == other.text_type) and (self.text == other.text) and (self.url == other.url):
-            return True
-        return False
+        return (self.text_type == other.text_type) and (self.text == other.text) and (self.url == other.url):
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
