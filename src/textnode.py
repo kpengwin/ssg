b17b996f4b49dfcf2a8f8aa610ccf96414fd654b
Kenneth Bruce <me@kenneth-bruce.com>
2025-05-24 14:06:02 -0400

    remove trailing :

--- src/textnode.py
+++ src/textnode.py
@@ -15,7 +15,7 @@ class TextNode():
         self.url = url
 
     def __eq__(self, other):
-        return (self.text_type == other.text_type) and (self.text == other.text) and (self.url == other.url):
+        return (self.text_type == other.text_type) and (self.text == other.text) and (self.url == other.url)
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
